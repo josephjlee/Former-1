cd4d347690703cab53567d38cf79ad9853f9675e
Marco Monteiro <marco@monstrosecompanhia.com>
2014-09-30 15:43:52 +0100

    Bootstrap support for double

--- Library/Views/double.php
+++ Library/Views/double.php
@@ -1,1 +1,4 @@
-<input type="text" name="<?= $name; ?>" class="former-<?= $name; ?> former-double" value="<?= $value; ?>">
+<div class="form-group">
+    <label for="<?= $name; ?>"><?= $name; ?></label>
+    <input type="text" name="<?= $name; ?>" class="form-control former-<?= $name; ?> former-double" id="<?= $name; ?>" value="<?= $value; ?>" maxlength="<?= $length; ?>" placeholder="10,00">
+</div>
