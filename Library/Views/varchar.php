a26a6217e35c882cc730d74570fa3f0730d9d088
Marco Monteiro <marco@monstrosecompanhia.com>
2014-09-30 14:21:57 +0100

    Boostrap 3.2.x support for varchar

--- Library/Views/varchar.php
+++ Library/Views/varchar.php
@@ -1,1 +1,4 @@
-<input type="text" name="<?= $name; ?>" class="former-<?= $name; ?> former-char" value="<?= $value; ?>" maxlength="<?= $length; ?>" placeholder="<?= $name; ?>">
+<div class="form-group">
+    <label for="<?= $name; ?>"><?= $name; ?></label>
+    <input type="text" name="<?= $name; ?>" class="form-control former-<?= $name; ?> former-char" id="<?= $name; ?>" value="<?= $value; ?>" maxlength="<?= $length; ?>" placeholder="<?= $name; ?>">
+</div>
