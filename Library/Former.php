035a471a09144d39c8a5beaf480b4be77ea6be68
Tomaz Lovrec <tomaz.lovrec@gmail.com>
2014-09-30 12:11:53 +0200

    Add moar types

--- Library/Former.php
+++ Library/Former.php
@@ -2,7 +2,7 @@
 /**
  * CodeIgniter form builder from database table column types.
  *
- *
+ * Needs a way better description
  *
  * @author Tomaz Lovrec <tomaz.lovrec@gmail.com>
  */
@@ -56,14 +56,48 @@ class Former
                 $form .= $this->_ci->load->view("former/hidden", $data, true);
             } else {
                 switch ($c->type) {
+                    case "char":
                     case "varchar":
                         $form .= $this->_ci->load->view("former/varchar", $data, true);
                         break;
+                    case "tinyint":
+                    case "smallint":
+                    case "mediumint":
+                    case "bigint":
                     case "int":
                         $form .= $this->_ci->load->view("former/int", $data, true);
                         break;
+                    case "double":
+                    case "float":
+                    case "decimal":
+                        $form .= $this->_ci->load->view("former/double", $data, true);
+                        break;
+                    case "tinytext":
+                    case "mediumtext":
+                    case "longtext":
                     case "text":
                         $form .= $this->_ci->load->view("former/text", $data, true);
+                        break;
+                    case "date":
+                        $form .= $this->_ci->load->view("former/date", $data, true);
+                        break;
+                    case "time":
+                        $form .= $this->_ci->load->view("former/time", $data, true);
+                        break;
+                    case "datetime":
+                        $form .= $this->_ci->load->view("former/datetime", $data, true);
+                        break;
+                    case "timestamp":
+                        $form .= $this->_ci->load->view("former/timestampe", $data, true);
+                        break;
+                    case "enum":
+                        $form .= $this->_ci->load->view("former/dropdown", $data, true);
+                        break;
+                    case "set":
+                        $form .= $this->_ci->load->view("former/dropdown", $data, true);
+                        break;
+                    case "bit":
+                        $form .= $this->_ci->load->view("former/radio", $data, true);
                         break;
                     default:
                         break;
